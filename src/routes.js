1876e32e67cde146991dba9e5d4f6f1fd123b3e4
Osos-7 <osos.amr2002@gmail.com>
2023-01-01 22:10:38 +0200

    register changed

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 
-const registerUser = require("./controllers/registration");
+const registerUser = require("./controllers/register");
 const getUser = require("./controllers/getUser");
 
 const router = express.Router();
 
-router.post("/registration", registerUser);
+router.post("/register", registerUser);
 router.post("/getUser", getUser);
 
 
